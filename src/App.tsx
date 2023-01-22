339b257341c78ac498e147d62dd07a8e6879f1b6
Dan Haggerty <daniel.r.haggerty@gmail.com>
2023-01-22 18:32:44 -0500

    Linted

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Dan's site coming soon!
-        </p>
+        <p>Dan's site coming soon!</p>
         <a
           className="App-link"
           href="https://reactjs.org"
