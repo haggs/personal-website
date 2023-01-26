ea454462ab7af42c5f293a26468e55bdb2e8d863
Dan Haggerty <daniel.r.haggerty@gmail.com>
2023-01-25 19:15:55 -0500

    Added footer and some placeholder navigation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
+import { container } from './index.module.css';
 import Seo from '../components/Seo/Seo';
 import Layout from '../components/Layout/Layout';
 
 const IndexPage: React.FC = () => {
   return (
     <Layout>
-      <p>This is a placeholder for my personal website, more to come, probably.</p>
-      <StaticImage
-        alt="Dan Haggerty, frontend developer"
-        src="../images/profile-pic.jpg"
-      />
+      <div className={container}>
+        <p>
+          This is a placeholder for my personal website, more to come, probably.
+        </p>
+        <StaticImage alt="Dan Haggerty" src="../images/profile-pic.jpg" />
+      </div>
     </Layout>
   );
 };
